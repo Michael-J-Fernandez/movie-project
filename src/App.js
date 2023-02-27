1777887485b71324de3b0d1e7f3cb702c3c4467f
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-02-27 12:45:58 -0500

    Imported data. Mapped data onto table component to display data in the browser.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import data from './data'
 
-function App() {
+export default function App() {
+
+  const [moviesData, setMoviesData] = useState(data)
+
+  function Head () {
+    return (
+      <thead>
+        <tr>
+          <th>title</th>
+          <th>actors</th>
+          <th>plot</th>
+          <th>genre</th>
+          <th>imdbRating</th>
+          <th>director</th>
+          <th>year</th>
+          <th>dateAdded</th>
+          <th>dateModified</th>
+          <th>modify</th>
+        </tr>
+      </thead>
+    )
+  }
+
+  function Body (props) {
+    
+    const { movies } = props
+    console.log("from the body: ", movies[0])
+    
+    const deleteRow = (index) => {
+
+    
+
+      return (
+        console.log(`Deleting index ${index}!`)
+
+        // setMoviesData(prevMoviesData => {
+        //   return {
+        //     ...prev,
+
+        //   }
+        // }
+        )
+      // )
+    }
+
+    return (
+      <tbody>
+        {movies.map((movie, index )=> (
+          <tr key={index}>
+            <td>{movie.title}</td>
+            <td>{movie.actors}</td>
+            <td>{movie.plot}</td>
+            <td>{movie.genre}</td>
+            <td>{movie.imdbRating}</td>
+            <td>{movie.director}</td>
+            <td>{movie.year}</td>
+            <td>{movie.dateAdded}</td>
+            <td>{movie.dateModified}</td>
+            <td><button onClick={() => deleteRow(index)}>Delete</button></td>
+          </tr>
+        ))}
+      </tbody>
+    )
+  }
+
+  // console.log(movies)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <Head />
+      <Body movies={moviesData}/>
+
+    </table>
   );
 }
-
-export default App;
