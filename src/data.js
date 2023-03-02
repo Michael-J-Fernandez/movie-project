76f0689a7b0f42a5089a4ed5230b77151ca01348
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-02 11:06:01 -0500

    Deleted  unnecessary comments.

--- src/data.js
+++ src/data.js
@@ -4,7 +4,6 @@ const movies = [
   {
     title: "The Godfather",
     actors: ["Marlon Brando", "Al Pacino", "James Caan"],
-    //-- plot: "Sample plot",
     plot: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
     genre: "Crime, Drama",
     imdbRating: 9.2,
@@ -15,7 +14,6 @@ const movies = [
   {
     title: "The Shawshank Redemption",
     actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
-    //-- plot: "Sample plot",
     plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
     genre: "Drama",
     imdbRating: 9.3,
@@ -26,7 +24,6 @@ const movies = [
   {
     title: "The Dark Knight",
     actors: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
-    //-- plot: "Sample plot",
     plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
     genre: "Action, Crime, Drama",
     imdbRating: 9.0,
@@ -37,7 +34,6 @@ const movies = [
   {
     title: "Pulp Fiction",
     actors: ["John Travolta", "Samuel L. Jackson", "Uma Thurman"],
-    //-- plot: "Sample plot",
     plot: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
     genre: "Crime, Drama",
     imdbRating: 8.9,
@@ -48,7 +44,6 @@ const movies = [
   {
     title: "The Lord of the Rings: The Return of the King",
     actors: ["Elijah Wood", "Viggo Mortensen", "Ian McKellen"],
-    //-- plot: "Sample plot",
     plot: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
     genre: "Action, Adventure, Drama",
     imdbRating: 8.9,
@@ -59,7 +54,6 @@ const movies = [
   {
     title: "Forrest Gump",
     actors: ["Tom Hanks", "Robin Wright", "Gary Sinise"],
-    //-- plot: "Sample plot",
     plot: "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
     genre: "Drama, Romance",
     imdbRating: 8.8,
@@ -70,7 +64,6 @@ const movies = [
   {
     title: "Inception",
     actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
-    //-- plot: "Sample plot",
     plot: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an ideainto the mind of a C.E.O.",
     genre: "Action, Adventure, Sci-Fi",
     imdbRating: 8.8,
@@ -81,7 +74,6 @@ const movies = [
   {
     title: "The Matrix",
     actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
-    //-- plot: "Sample plot",
     plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
     genre: "Action, Sci-Fi",
     imdbRating: 8.7,
@@ -92,7 +84,6 @@ const movies = [
   {
     title: "Schindler's List",
     actors: ["Liam Neeson", "Ben Kingsley", "Ralph Fiennes"],
-    //-- plot: "Sample plot",
     plot: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
     genre: "Biography, Drama, History",
     imdbRating: 8.9,
@@ -103,7 +94,6 @@ const movies = [
   {
     title: "Goodfellas",
     actors: ["Robert De Niro", "Ray Liotta", "Joe Pesci"],
-    //-- plot: "Sample plot",
     plot: "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
     genre: "Crime, Drama",
     imdbRating: 8.7,
@@ -114,142 +104,3 @@ const movies = [
 ];
 
 export default movies
-
-
-/*
-TESTING DATA IN THIS SECTION
-DISREGARD!
-
-const movies = [
-  {
-    title: "The Godfather",
-    actors: ["Marlon Brando", "Al Pacino", "James Caan"],
-    //-- plot: "Sample plot",
-    plot: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-    genre: "Crime, Drama",
-    imdbRating: 9.2,
-    director: "Francis Ford Coppola",
-    year: 1972,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "The Shawshank Redemption",
-    actors: ["Tim Robbins", "Morgan Freeman", "Bob Gunton"],
-    //-- plot: "Sample plot",
-    plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-    genre: "Drama",
-    imdbRating: 9.3,
-    director: "Frank Darabont",
-    year: 1994,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "The Dark Knight",
-    actors: ["Christian Bale", "Heath Ledger", "Aaron Eckhart"],
-    //-- plot: "Sample plot",
-    plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-    genre: "Action, Crime, Drama",
-    imdbRating: 9.0,
-    director: "Christopher Nolan",
-    year: 2008,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "Pulp Fiction",
-    actors: ["John Travolta", "Samuel L. Jackson", "Uma Thurman"],
-    //-- plot: "Sample plot",
-    plot: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-    genre: "Crime, Drama",
-    imdbRating: 8.9,
-    director: "Quentin Tarantino",
-    year: 1994,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "The Lord of the Rings: The Return of the King",
-    actors: ["Elijah Wood", "Viggo Mortensen", "Ian McKellen"],
-    //-- plot: "Sample plot",
-    plot: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.",
-    genre: "Action, Adventure, Drama",
-    imdbRating: 8.9,
-    director: "Peter Jackson",
-    year: 2003,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "Forrest Gump",
-    actors: ["Tom Hanks", "Robin Wright", "Gary Sinise"],
-    //-- plot: "Sample plot",
-    plot: "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
-    genre: "Drama, Romance",
-    imdbRating: 8.8,
-    director: "Robert Zemeckis",
-    year: 1994,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "Inception",
-    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page"],
-    //-- plot: "Sample plot",
-    plot: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an ideainto the mind of a C.E.O.",
-    genre: "Action, Adventure, Sci-Fi",
-    imdbRating: 8.8,
-    director: "Christopher Nolan",
-    year: 2010,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "The Matrix",
-    actors: ["Keanu Reeves", "Laurence Fishburne", "Carrie-Anne Moss"],
-    //-- plot: "Sample plot",
-    plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
-    genre: "Action, Sci-Fi",
-    imdbRating: 8.7,
-    director: "Lana Wachowski, Lilly Wachowski",
-    year: 1999,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "Schindler's List",
-    actors: ["Liam Neeson", "Ben Kingsley", "Ralph Fiennes"],
-    //-- plot: "Sample plot",
-    plot: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
-    genre: "Biography, Drama, History",
-    imdbRating: 8.9,
-    director: "Steven Spielberg",
-    year: 1993,
-    dateAdded: "dateAdded-placeholder",
-  },
-  {
-    title: "Goodfellas",
-    actors: ["Robert De Niro", "Ray Liotta", "Joe Pesci"],
-    //-- plot: "Sample plot",
-    plot: "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate.",
-    genre: "Crime, Drama",
-    imdbRating: 8.7,
-    director: "Martin Scorsese",
-    year: 1990,
-    dateAdded: "dateAdded-placeholder",
-  },
-];
-
-
-const query = {
-  searchText: "The Godfather",
-  searchCategory: "title",
-};
-
-let category = JSON.stringify(query.searchCategory)//?
-let text = JSON.stringify(query.searchText)
-
-console.log(movies[0][`${query.searchCategory}`])
-
-const filteredMovies = movies.filter(movie => {
-    return movie[`${query.searchCategory}`].toLowerCase()
-    .includes(query.searchText.toLowerCase());
-})
-
-filteredMovies//?
-
-
-*/
