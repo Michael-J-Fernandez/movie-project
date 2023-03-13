9f46d348341140aeda71f2b21f103c467a754b9c
Michael Fernandez <112970281+Michael-J-Fernandez@users.noreply.github.com>
2023-03-12 23:39:44 -0400

    Replaced BrowserRouter with createBrowserRouter. Added route for SingleMovie. Added errorElement to root route.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css"
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from "react-router-dom";
 
 import Layout from './layouts/Layout'
 import Home from './pages/Home'
 import MovieForm from "./components/MovieForm";
 import Table from "./components/Table";
+import SingleMovie from "./pages/SingleMovie";
+import Error from "./components/Error"
 
 
 const DATA_URL = "https://raw.githubusercontent.com/Michael-J-Fernandez/movie-project/00953d3a1e0683a198f6f79072721f221e68b657/src/data-copy.json";
 
+
 export default function App() {
-  
+
   // /* --- MOVE THIS CODE TO Table
   const [moviesData, setMoviesData] = useState([]);
 
@@ -19,7 +28,6 @@ export default function App() {
     fetch(DATA_URL)
       .then(res => res.json())
       .then(data => setMoviesData(data))
-
   }, [])
 
   // Add user data to moviesDataState
@@ -33,15 +41,29 @@ export default function App() {
     setMoviesData((prevMoviesData) => [...prevMoviesData, newMovieData]);
   };
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Table moviesData={moviesData} setMoviesData={setMoviesData} />} /> 
-           <Route path="movies/:id" element={<MovieForm addMovie={addMovie} />} />
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />} errorElement={<Error />}>
+        <Route index element={<Home />} />
+        <Route
+          path="movies"
+          element={
+            <Table moviesData={moviesData} setMoviesData={setMoviesData} />
+          }
+        >
+          <Route index element={<MovieForm addMovie={addMovie} />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
+        <Route
+          path="movies/:singleTitle"
+          element={<SingleMovie moviesData={moviesData} />}
+        />
+      </Route>
+    )
+  );
+
+
+  return (
+    <RouterProvider router={router} />
   );
 }
